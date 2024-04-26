a92519dff486930d67bc8e0f0444dd64557f6fb5
Скоківський Володимир <ipz203_svv@student.ztu.edu.ua>
2024-04-27 02:07:33 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import {NavigationContainer} from "@react-navigation/native";
+
+export default function App() {
+return (
+        <NavigationContainer>
+            <View style={styles.container}>
+                <Header
+                    title="Лабораторна робота"
+                    subTitle="Скоківський Володимир ІПЗ-20-3"
+                    imageUrl={require('./assets/logo_zdtu.png')}
+                />
+                <Navigation />
+                <StatusBar style="auto" />
+            </View>
+        </NavigationContainer>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ config.js
@@ -0,0 +1,6 @@
+const Config = {
+    newsUrl: 'https://raw.githubusercontent.com/enot1k666/LabsMobileApp/master/data/news.json',
+    galleryUrl: 'https://raw.githubusercontent.com/enot1k666/LabsMobileApp/master/data/images.json',
+};
+
+export default Config;
